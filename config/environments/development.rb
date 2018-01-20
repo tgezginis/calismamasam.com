b28b2614daddb24cbcc20459b8e9d8a864ecebc7
=roguearch <=sametaylak29@gmail.com>
2018-01-21 00:35:28 +0300

    Changed stmp provider and password reset error handling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,12 +63,5 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost:5000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { 
-    address: 'smtp.gmail.com', 
-    port: 587,
-    user_name: ENV['EMAIL_USERNAME'],
-    password: ENV['EMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
 end
